eb6d85f3ad232dc67da031b5159183c2d499e7d0
Vina del Mar <vina@duoc.acad>
2025-05-12 21:01:24 -0400

    holi

---
+++ src/main/java/cl/duoc/mcsv_envios/McsvEnviosApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.mcsv_envios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McsvEnviosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McsvEnviosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cl/duoc/mcsv_envios/McsvEnviosApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.mcsv_envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class McsvEnviosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
