a69c206b49a9eba3333ee3420493bd05a9897d83
Vina del Mar <vina@duoc.acad>
2025-05-30 20:11:29 -0400

    refactor rename paquete test

--- src/test/java/cl/duoc/mcsv_pedidos/McsvEnviosApplicationTests.java
+++ src/test/java/cl/duoc/mcsv_pedidos/McsvEnviosApplicationTests.java
@@ -1,4 +1,4 @@
-package cl.duoc.mcsv_envios;
+package cl.duoc.mcsv_pedidos;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ src/test/java/cl/duoc/mcsv_pedidos/model/dto/converter/DetallePedidoConverterTest.java
@@ -0,0 +1,16 @@
+package cl.duoc.mcsv_pedidos.model.dto.converter;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DetallePedidoConverterTest {
+
+    @Test
+    void convert() {
+    }
+
+    @Test
+    void convertToDTOList() {
+    }
+}
